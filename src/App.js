9a2d64cab442c75cce2df185715b7bc30971fdd7
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-29 16:12:00 -0500

    Feat: Add inputs for meme search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Navbar from "./components/Navbar";
+import Inputs from "./components/Inputs";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App container max-w-full">
+    <div className="App container min-w-full">
       <header className="App-header shadow-2xl h-16 p-2">
         <Navbar />
       </header>
+      <main className="pt-6 h-screen bg-white p-2 max-w-lg md:max-w-4xl mx-auto">
+        <Inputs />
+      </main>
     </div>
   );
 }
---
+++ src/components/Inputs.js
@@ -0,0 +1,33 @@
+const Forms = () => {
+  return (
+    <div className="flex items-center justify-evenly md:justify-around">
+      <div className="">
+        <label className="block text-xs  font-black md:text-base" for="email">
+          {" "}
+          Top Meme{" "}
+        </label>
+
+        <input
+          className=" md:w-auto px-3 py-1 mt-1 text-sm  border-2 border-gray-200 rounded"
+          id="email"
+          type="email"
+        />
+      </div>
+      {/* input */}
+      <div className="">
+        <label className="block text-xs font-black  md:text-base" for="email">
+          {" "}
+          Bottom Meme{" "}
+        </label>
+
+        <input
+          className=" md:w-auto px-3 py-1 mt-1 text-sm border-2 border-gray-200 rounded"
+          id="email"
+          type="email"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Forms;
