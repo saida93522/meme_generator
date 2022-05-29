25c4dbe51c31258c120a17ee16e56406be2265df
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-29 14:48:03 -0500

    Style:Add Fixed Height to Header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App container max-w-full">
-      <header className="App-header shadow-2xl">
+      <header className="App-header shadow-2xl h-16 p-2">
         <Navbar />
       </header>
     </div>
