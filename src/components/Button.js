f99855f9011f025dde08a4990f6b8f5b91c77d53
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-30 11:19:59 -0500

    refactor: change a tag to btn tag

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,20 @@
 const Button = () => {
+  const handleClick = () => {
+    console.log("Click");
+  };
   return (
-    <div className="mt-4 w-3/4 mx-auto">
-      <a
-        className="flex items-center justify-center px-0 py-2  border rounded-lg  group focus:outline-none focus:ring meme-btn"
-        href="/download"
+    <div className="mt-4 w-3/4 mx-auto ">
+      <button
+        onClick={handleClick}
+        className="flex items-center justify-center w-3/4 py-2  border rounded-lg mx-auto  px-3 focus:outline-none focus:ring meme-btn"
+        href="#"
       >
         <span className="font-bold text-white ">Get a new meme image </span>
 
         <span className="flex-shrink-0 p-1 ml-4 text-indigo-600 bg-white border border-current rounded-full group-active:text-indigo-500">
           <img src="btn-icon.png" className="btn-img" alt="logo" />
         </span>
-      </a>
+      </button>
     </div>
   );
 };
