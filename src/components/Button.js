09c50301f48e0ce4863224368c3bcea9c42e7db9
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-31 14:24:06 -0500

    feat:display random meme image

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,20 @@
+import memeData from "../MemeApi";
+import { useState } from "react";
 const Button = () => {
-  const handleClick = () => {
-    console.log("Click");
+  const [memeImage, setMemeImage] = useState("");
+
+  const getMemeImage = () => {
+    const memeArray = memeData.data.memes;
+    const randomMeme = [Math.floor(Math.random() * memeArray.length)];
+    const url = memeArray[randomMeme].url;
+    setMemeImage(url);
   };
+
   return (
     <div className="mt-4 w-3/4 mx-auto ">
       <button
-        onClick={handleClick}
-        className="flex items-center justify-center w-3/4 py-2  border rounded-lg mx-auto  px-3 focus:outline-none focus:ring meme-btn"
-        href="#"
+        onClick={getMemeImage}
+        className="flex items-center justify-center w-3/4 py-2  border rounded-lg mx-auto px-3 focus:outline-none focus:ring meme-btn"
       >
         <span className="font-bold text-white ">Get a new meme image </span>
 
@@ -15,6 +22,21 @@ const Button = () => {
           <img src="btn-icon.png" className="btn-img" alt="logo" />
         </span>
       </button>
+      <div
+        style={{
+          backgroundImage: `url('${memeImage}')`,
+          backgroundPosition: "center",
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          width: 600,
+          height: 600,
+        }}
+        className="relative block overflow-hidden mx-auto mt-16"
+      >
+        <span className="absolute z-10 text-center inset-0  text-white">
+          Top Meme
+        </span>
+      </div>
     </div>
   );
 };
